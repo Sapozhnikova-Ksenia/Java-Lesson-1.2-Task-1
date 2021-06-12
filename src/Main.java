8eb8bcb56a051839a937a3146f70de1513332bdc
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-06-12 19:41:58 +0300

    Main.java added and testing

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanceCurrent = 2000000000;
+        int balanceReplenishment = 500000000;
+        int balanceTotal = balanceCurrent + balanceReplenishment;
+        System.out.println(balanceTotal);
+    }
+}
